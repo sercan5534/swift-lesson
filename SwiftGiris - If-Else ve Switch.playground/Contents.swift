8526a4f5d3a91ff5ad28397061d10fc36988f198
sozen <sozen@netas.com.tr>
2016-10-30 23:21:56 +0200

    If-Else ve switch

---
+++ SwiftGiris - If-Else ve Switch.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+var a = 1
+
+if a == 1 {
+    print("if")
+}
+else{
+    print("else")
+}
+
+switch a {
+case 1:
+    print("sdsd")
+default:
+    print("default")
+}
+
+
